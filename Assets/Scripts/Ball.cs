724950b12b90d52a5523991b279a56414ab3ae3c
greenfieldxd <monsterbig53@gmail.com>
2020-03-05 21:14:46 +0300

    Add ball, platform, walls, block(prefab) etc.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,16 +4,50 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float speedBall;
+
+    bool started;
+    Platform platform;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        started = false;
+        platform = FindObjectOfType<Platform>(); //Найти Platform на сцене
+        rb = GetComponent<Rigidbody2D>(); // найти компонет Rigitbody2D на том же гейм обжекте
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (started)
+        {
+            //ничего не делаем
+            Debug.Log(rb.velocity.magnitude);  //сколько юнитов в секунду пролетает мяч
+        }
+        else
+        {
+            LockBallToPlatform();
+        }
+    }
+
+    private void LockBallToPlatform()
+    {
+        //двигаться с платформой
+        transform.position = new Vector3(platform.transform.position.x, transform.position.y, 0);
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Запуск мяча
+            started = true;
+            LaunchBall();
+        }
+    }
+
+    private void LaunchBall()
+    {
+        Vector2 force = new Vector2(speedBall, speedBall);
+        rb.AddForce(force);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float minX; // Максимальное и минимальное X координат платформы
+    public float maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;// позиция мыши в координатах камеры
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos); // позиция мыши в координатах мира
+        //Debug.Log("mousePos: " + mousePos + ", mouseWorldPos: " + mouseWorldPos);
+
+        float xPlatform = mouseWorldPos.x;// значение x координат мыши
+        float clampedPlatformX = Mathf.Clamp(xPlatform, minX, maxX); // обрезаем координаты платформы по x до нужных нам значений
+        float yPlatform = transform.position.y;
+
+        transform.position = new Vector3(clampedPlatformX, yPlatform, 0);
+    }
+}
