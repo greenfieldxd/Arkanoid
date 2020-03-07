d899037d9e35a823f2fb85ee2634f1adf215be4f
greenfieldxd <monsterbig53@gmail.com>
2020-03-07 23:04:04 +0300

    Add Level 1-1, add block levels

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,14 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 [RequireComponent(typeof(Collider2D))]
 public class Block : MonoBehaviour
 {
+    public Sprite[] spriteLevel;
+
+    public int strength;
+
+
+
+    public void Start()
+    {
+        ChangeSprite(spriteLevel.Length - 1);
+    }
+
+    private void ChangeSprite(int index)
+    {
+        SpriteRenderer sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.sprite = spriteLevel[index];
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+
+        if (strength == 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            strength--;
+            ChangeSprite(strength);
+
+        }
     }
 }
